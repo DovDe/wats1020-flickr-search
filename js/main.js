2f66884e2cac76f05fc1bdbf2efff5fc06cde0bc
dovdekeyser <dovdekeyser@yahoo.com>
2017-02-13 01:56:04 -0500

    updated js html and css

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
-// Asynchronous Flickr Search
-//
-// Flickr reveals a searchable JSON Feed you can access via jQuery's $.getJSON()
-// method. Use this to allow users to search for a tag or comma-separated list
-// of tags and view the images that are found.
-//
-// Allow users to click the images to see a larger version with more information.
+
 $(document).on('ready', function(){
-    // Place your code here, inside the document ready handler.
-
-    // Create a function called `searchImages()`. This function will handle the
-    // process of taking a user's search terms and sending them to Flickr for a
-    // response.
-
+    
+  
+  
+// this function searches for flicker images 
 var searchImages = function(tags){
   
  
-  
+  // this goes to the flickr data base and gets the data
   (function() {
     
   var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
@@ -29,116 +21,156 @@ var searchImages = function(tags){
   
     .done(function( data ) {
     
-
+// this empties out the older search content 
     $('#images').empty();
+    
     
       $.each( data.items, function( i, item ) {
         
-     var newItem = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 new-item">');  
-     var newP = $('<p class="">');   
-  
-      var newImage = $( "<img class='img-thumbnail avatar'>" ).attr( "src", item.media.m );
- 
+        // these next lines I am creating the bootstrap divs to place my content in 
       
-      var moreInfoButton = $("<button>").text("More Info"); 
-      $(moreInfoButton).addClass("btn btn-primary btn-lg more-info");
-     
-        $(moreInfoButton).attr({'data-toggle': 'modal',
-                               'data-target' : '#infoModal',
-                                'data-title' : item.title, 
-                                'data-author' : item.author,
-                                'data-imgsrc' : item.media.m,
-                                'data-description': item.description,
+       var newItem = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 new-item">');
+        
+        
+        
+        
+       var imageRowDiv  =  $('<div class="row">');
+       var imageColDiv = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">');        
+       var newImage = $( "<img class='img-thumbnail img-responsive image'>" ).attr( "src", item.media.m );
+        
+ 
+      var buttonsRowDiv = $('<div class="row ">'); 
+      var buttonsColDiv = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">');  
+  
+        
+       var descriptionInfoButton =  $("<button type='button' class='btn btn-primary btn-block descriptionInfo ' data-toggle='modal' data-target='#infoModal' data-whatever='@description'>Open modal for @mdo>").text("Description");
+       var authorInfoButton =  $("<button type='button' class='btn btn-primary btn-block authorInfo' data-toggle='modal' data-target='#infoModal' data-whatever='@author'>Open modal for @mdo>").text("Author Info");
+       var sourceInfoButton =  $("<button type='button' class='btn btn-primary btn-block sourceInfo' data-toggle='modal' data-target='#infoModal' data-whatever='@mdo'>Open modal for @mdo>").text("Source Info");
+ 
+
+        // these next lines are the button data for my modals
+        
+        $(descriptionInfoButton).attr({'data-toggle': 'modal',
+                                'data-target' : '#infoModal',
+                                'data-title' : item.title,              
+                                'data-description': item.description,                                
+                                'data-button' : descriptionInfoButton,
+                                 'type' : 'button'                                  
                                });
        
       
+        $(authorInfoButton).attr({
+                        'data-toggle': 'modal',
+                        'data-target' : '#infoModal',
+                        'data-author' : item.author,
+                        'data-title' : item.title, 
+                        'data-description': item.description,
+                        'data-button' : authorInfoButton,
+                        'type' : 'button'
+                          });
+        
+        $(sourceInfoButton).attr({
+                        'data-toggle': 'modal',
+                        'data-target' : '#infoModal',
+                        'data-title' : item.title, 
+                        'data-imgsrc' : item.media.m,
+                        'data-description': item.description,
+                        'data-button' : sourceInfoButton,
+                        'type' : 'button'           
+                          });
      
+      
+       // these are the variables I am using to fill the divs into the correct containers
+       // currently I do not actually need to write the code this way but I am trying to come uo with 
+       // some function the will enable me to account for the bootstrap row and columns and fill them acording to the
+        //break points. 
         
+   var addImage =      newImage.appendTo(imageColDiv).appendTo(imageRowDiv).appendTo(newItem);
+   var addButton =      buttonsColDiv.appendTo(buttonsRowDiv).appendTo(newItem);
+   var descriptionButton   =   descriptionInfoButton.appendTo(buttonsColDiv);
+    var authorButton  =    authorInfoButton.appendTo(buttonsColDiv);
+    var sourceButton =    sourceInfoButton.appendTo(buttonsColDiv);   
+         
+    
+       // here I am actually appending the items into their correct divs  
         
-      newImage.appendTo(newItem); 
-      newP.appendTo(newItem); 
-      moreInfoButton.appendTo(newItem);   
-      $('#images').append(newItem);
+    var newRow = $('<div class="row new-Row">');
+           
+       
+    addImage; addButton; descriptionButton; authorButton; sourceButton;
+    newItem.appendTo(newRow);   
+    
+    $('#images').append(newRow);
         
-        
-        
-        if ( i === 10 ) {
+        if ( i === 16 ) {
           return false;
         }
+        
+        
+        
       });
+
     });  
   })();
-    
+   
   
 };
 
 
   
-  
-    // Inside the `searchImages()` function, the following things should happen:
-
-        // 1.   Accept a string value called `tags` as an argument. Example:
-        //      `var searchPhotos = function(tags){`
-
-        //
-        // 2.   Define the location of the Flickr API like this:
-        //      `var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";`
-        //
-        // 3.   Construct a `$.getJSON()` call where you send a request object
-        //      including the tags the user submitted, and a `done()` handler
-        //      that displays and refreshes the content appropriately.
-        //
-        // 4.   Update the display to add the images to the list with the id
-        //      `#images`.
-
-    // Attach an event to the search button (`button.search`) to execute the
-    // search when clicked.
-
+// this is to activate the search button
   
   $('button.search').click(function(){
     
-     
-    event.preventDefault();
+     // this prevents any defaults that would happen apoun clicking on the button
     
+    event.preventDefault();
+   
+    
+  // this finds the text input and names it searchtags
+    //then calls the function using the searchimages function
  var searchTags = $("input:text").val();  
    searchImages(searchTags);
     
   });
   
+  
+  // this function is to create the modals 
 $('#infoModal').on('show.bs.modal', function (event) {
+  
+  
   var button = $(event.relatedTarget) // Button that triggered the modal
   var imageTitle = button.data('title'); // Extract info from data-* attributes
   var imageAuthor = button.data('author');
   var imageDescription = button.data('description');
+  var imageSource = button.data('imgsrc');
   console.log(imageTitle)
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  
+  
   var modal = $(this);
   
+  // here I am emptying out the modals
   modal.find('.modal-title').empty();
   modal.find('.modal-body').empty();
-
-  modal.find('.modal-title').html('<h2>Image Title</h2>' + imageTitle);
-
-  modal.find('.modal-body').html('<h3>Image Author</h3>' + imageAuthor + '<br>' + '<h3>Image Description</h3>' + imageDescription );
+  
+   modal.find('.modal-title').html(imageTitle);
   
   
+// here I am finding the correct button and activating it and filling it 
+  // with the correct data
+  
+if ($(button).hasClass('descriptionInfo')){
+  console.log('this rocks');
+   modal.find('.modal-body').html('<h3>Image Description</h3>' + imageDescription );
+} else if ($(button).hasClass('authorInfo')) {
+  modal.find('.modal-body').html('<h3>Author</h3>' + imageAuthor );
+}else {
+    modal.find('.modal-body').html('<h3>Source</h3>' + imageSource );
+}
 
-})
 
-        // When the Search button is clicked, the following should happen:
-        //
-        // 1.   Prevent the default event execution so the browser doesn't
-        //      Example: `event.preventDefault();`
-        //
-        // 2.   Get the value of the 'input[name="searchText"]' and use that
-        //      as the `tags` value you send to `searchImages()`.
-        //
-        // 3.   Execute the `searchImages()` function to fetch images for the
-        //      user.
+});
 
-    // STRETCH GOAL: Add a "more info" popup using the technique shown on the
-    // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
 
 
 
